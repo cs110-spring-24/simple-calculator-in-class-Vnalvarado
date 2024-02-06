45b6d72c6da320bcea2dfcde2bc47eefc0b8fd6d
Vnalvarado <157657964+Vnalvarado@users.noreply.github.com>
2024-02-05 16:18:57 -0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,19 @@
+num1=int(input("enter your num1:"))
+op=input("Enter your operator:")
+num2=int(input("Enter your num 2:"))
 
+if op=="+":
+	total = num1 + num2
+	print(f"{num1}+{num2} = {total}")
+elif op=="-":
+	print(f"{num1}-{num2} = {num1 - num2}")
+elif op == "*":
+	print(f"{num1}*{num2} = {num1 * num2}")
+elif op == "/":
+	print(f"{num1} / {num2} = {num1 / num2}")
+elif op == "**":
+	print(f"{num1} ** {num2} = {num1 ** num2}")
+elif op == "//":
+	print(f"{num1} // {num2} = {num1 // num2}")
+elif op == "%":
+	print(f"{num1} % {num2} = {num1 % num2}")
